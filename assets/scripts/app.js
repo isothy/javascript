bd3ec06942812007fc6a3c0249a6adef1ab910c7
isothy <isothy@yandex.com>
2019-11-26 22:26:57 +0300

    add new features

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -27,36 +27,51 @@ function writeToLog(
   console.log(logEntries);
 }
 
-function add() {
+function calculateResult(calculationType) {
+  if (
+    calculationType !== 'ADD' &&
+    calculationType !== 'SUBTRACT' &&
+    calculationType !== 'MULTIPLY' &&
+    calculationType !== 'DIVIDE' ||
+    !enteredNumber
+    ) {
+    return;
+  }
+
   const enteredNumber = getUserNumberInput();
   const initialNumber = currentResult;
-  currentResult += enteredNumber;
-  createAndWriteOutput('+', initialNumber, enteredNumber);
-  writeToLog('ADD', initialNumber, enteredNumber, currentResult);
+  let mathOperator;
+  if (calculationType === 'ADD') {
+    currentResult += enteredNumber;
+    mathOperator = '+';
+  } else if (calculationType === 'SUBTRACT') {
+    currentResult -= enteredNumber;
+    mathOperator = '-';
+  } else if (calculationType === 'MULTIPLY') {
+    currentResult *= enteredNumber;
+    mathOperator = '*';
+  } else if (calculationType === 'DIVIDE') {
+    currentResult /= enteredNumber;
+    mathOperator = '/';
+  }
+  createAndWriteOutput(mathOperator, initialNumber, enteredNumber);
+  writeToLog(calculationType, initialNumber, enteredNumber, currentResult);
+}
+
+function add() {
+  calculateResult('ADD');
 }
 
 function subtract() {
-  const enteredNumber = getUserNumberInput();
-  const initialNumber = currentResult;
-  currentResult -= enteredNumber;
-  createAndWriteOutput('-', initialNumber, enteredNumber);
-  writeToLog('SUBTRACT', initialNumber, enteredNumber, currentResult);
+  calculateResult('SUBTRACT');
 }
 
 function multiply() {
-  const enteredNumber = getUserNumberInput();
-  const initialNumber = currentResult;
-  currentResult *= enteredNumber;
-  createAndWriteOutput('*', initialNumber, enteredNumber);
-  writeToLog('MULTIPLY', initialNumber, enteredNumber, currentResult);
+  calculateResult('MULTIPLY');
 }
 
 function divide() {
-  const enteredNumber = getUserNumberInput();
-  const initialNumber = currentResult;
-  currentResult /= enteredNumber;
-  createAndWriteOutput('/', initialNumber, enteredNumber);
-  writeToLog('DIVIDE', initialNumber, enteredNumber, currentResult);
+  calculateResult('DIVIDE');
 }
 
 addBtn.addEventListener('click', add);
